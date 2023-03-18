3e5e43f3ac79e94321090ee2700cc5a14fff18ea
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-03-18 20:17:41 +0200

    Create-render-layout

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// 2.Реалізація делегування на ul.gallery і отримання url великого зображення.
+// 3.Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс
+//  jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
+
+//4. Відкриття модального вікна по кліку на елементі галереї. 
+// Для цього ознайомся з документацією і прикладами.
+
+//5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям.
+// Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
+const galleryListEl = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryMarkup)
+
+function createGallery(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </li>`;
+    }).join('');
+ 
+}; 
+
+// console.log(galleryItems);
